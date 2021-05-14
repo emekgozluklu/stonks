6db2c91fc2f7bed6d8ef603281d7425ef3b3895f
Emek Gözlüklü <gozlukl16@itu.edu.tr>
2021-05-15 01:35:05 +0300

    refactor group by date script

--- data_preparation/group_by_date.py
+++ data_preparation/group_by_date.py
@@ -4,20 +4,24 @@ import joblib
 import os
 
 # set read and write data paths
-data_path = os.path.join("..", "data", "imputed", "1min")
-write_data_path= os.path.join("..", "data", "imputed", "grouped_by_date")
+DATA_PATH = os.path.join("..", "data", "imputed_1min")
+WRITE_DATA_PATH = os.path.join("..", "data", "grouped_by_date")
 
 # get the list of files to be read
-files = os.listdir(data_path)
+try:
+    files = os.listdir(DATA_PATH)
+except FileNotFoundError:
+    raise Exception("You shoul first run the imputer.")
 
 # create write directory
 try:
-    os.mkdir(write_data_path)
+    os.mkdir(WRITE_DATA_PATH)
 except FileExistsError:
     pass
 
 
 def add_date_column(df):
+    """ Add date column to the given dataframe to make easier grouping data by date. """
     try:
         df["date"] = df["date_time"].apply(lambda x: x[:10])
     except KeyError:
@@ -25,22 +29,29 @@ def add_date_column(df):
     return df
 
 
-# create joblib
-for file in files:
+def get_label_from_filename(filename):
+    return filename.split()[0]
 
-    if not file.endswith(".csv"):
-        continue
 
-    label = file.split(".")[0]
-    print("Processing: ", label)
-
-    df = pd.read_csv(os.path.join(data_path,file))
+def group_data_by_date(file):
+    df = pd.read_csv(os.path.join(DATA_PATH, file))
 
     df = add_date_column(df)
-
     grouped = OrderedDict()
+
     for i in df.groupby("date"):
         grouped[i[0]] = i[1]
+    return grouped
 
-    joblib.dump(grouped, os.path.join(write_data_path, label + ".joblib"))
 
+if __name__ == "__main__":
+    for file in files:
+
+        if not file.endswith(".csv"):
+            continue
+
+        label = get_label_from_filename(file)
+        print("Processing: ", label)
+
+        grouped_data = group_data_by_date(file)
+        joblib.dump(grouped_data, os.path.join(WRITE_DATA_PATH, label[:-4] + ".joblib"))

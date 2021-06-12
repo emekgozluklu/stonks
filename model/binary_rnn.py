69d945245049ed6288bfb47dc6510a9e98711aa3
Emek Gözlüklü <gozluklu16@itu.edu.tr>
2021-06-12 17:18:46 +0300

    add model testing

--- model/binary_rnn.py
+++ model/binary_rnn.py
@@ -3,17 +3,24 @@ from tensorflow.keras.layers import Embedding, Dense, SimpleRNN, Flatten, LSTM
 import pandas as pd
 import os
 
+
 class RNN:
     def __init__(self, tensor_path, prediction_offset=30, labeling_threshold=0.01):
 
         self.tensor_path = tensor_path
         self.tensor = self.read_tensor_from_path()
+        self.number_of_rows = self.tensor.shape[0]
+        self.training_split = 0.9
+        self.training_rows = round(self.training_split * self.number_of_rows)
 
         self.features = self.tensor.shape[0]
         self.embedding_dim = 8
 
         self.x_train = self.get_x_train()
         self.y_train = self.get_y_train()
+
+        self.x_test = self.get_x_test()
+        self.y_test = self.get_y_test()
 
         self.history = None
 
@@ -42,18 +49,24 @@ class RNN:
     def build_model(self):
 
         model = Sequential()
-        model.add(Embedding(110, 32))
-        model.add(Flatten())
-        model.add(Dense(32, activation="relu"))
+        model.add(Dense(96, activation="relu"))
+        model.add(Dense(64, activation="relu"))
+        model.add(Dense(16, activation="relu"))
         model.add(Dense(1, activation='sigmoid'))
 
         return model
 
     def get_x_train(self):
-        return self.tensor[self.tensor.columns[:-1]]
+        return self.tensor[self.tensor.columns[:-1]][:self.training_rows]
 
     def get_y_train(self):
-        return self.tensor[self.tensor.columns[-1]]
+        return self.tensor[self.tensor.columns[-1]][:self.training_rows]
+
+    def get_x_test(self):
+        return self.tensor[self.tensor.columns[:-1]][self.training_rows:]
+
+    def get_y_test(self):
+        return self.tensor[self.tensor.columns[-1]][self.training_rows:]
 
     def run_model(self, save=True):
         self.model.compile(
@@ -63,10 +76,15 @@ class RNN:
         )
 
         self.history = self.model.fit(self.x_train, self.y_train,
-                                      epochs=5, batch_size=128)
+                                      epochs=5, batch_size=128, validation_split=0.1)
+
+        print("Evaluate on test data")
+        results = self.model.evaluate(self.x_test, self.y_test, batch_size=128)
+        print("test loss, test acc:", results)
 
         if save:
             self.model.save_weights(os.path.join("..", "data", "model.h5"))
+
 
 if __name__ == "__main__":
     rnn_model = RNN("..\\data\\tensor.joblib")
